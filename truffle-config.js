8c8e1b65e41583e7808d02968af47c05dd8fae11
hoang <vinhyenvodoi98@gmail.com>
2019-09-14 19:18:55 +0700

    update research

--- truffle-config.js
+++ truffle-config.js
@@ -2,9 +2,11 @@ const { readFileSync } = require('fs');
 const LoomTruffleProvider = require('loom-truffle-provider');
 
 const chainId = 'default';
-const writeUrl = 'http://172.17.0.2:46658/rpc';
-const readUrl = 'http://172.17.0.2:46658/query';
-const privateKey = readFileSync('./private_key', 'utf-8');
+const writeUrl = 'http://172.17.0.3:46658/rpc';
+const readUrl = 'http://172.17.0.3:46658/query';
+// const writeUrl = 'http://127.0.0.1:46658/rpc';
+// const readUrl = 'http://127.0.0.1:46658/query';
+const privateKey = readFileSync('./priv_key', 'utf-8');
 
 const loomTruffleProvider = new LoomTruffleProvider(
   chainId,
